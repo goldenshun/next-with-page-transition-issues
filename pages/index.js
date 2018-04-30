1fc5c3a07395f43b09dc53b49a90e0b070a9b216
Sean Connolly <sconnolly17@gmail.com>
2018-04-30 11:00:27 -0600

    Sample issues

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default () => (
+  <div>
+    <h1>Index</h1>
+    <Link href="/other"><a>Other</a></Link>
+    <Link href="/another"><a>Another</a></Link>
+  </div>
+)
---
+++ pages/other.js
@@ -0,0 +1,8 @@
+import Link from 'next/link';
+
+export default () => (
+  <div>
+    <h1>Other</h1>
+    <Link href="/"><a>Home</a></Link>
+  </div>
+)
